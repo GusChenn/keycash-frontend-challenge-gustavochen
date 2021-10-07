578d9b1e142d22af89534795c6db0e4873aa816e
Gustavo Chen <gustavo.sao@gmail.com>
2021-10-07 18:32:52 -0300

    Elaboração da estrutura de Vitrine

--- src/components/Vitrine.jsx
+++ src/components/Vitrine.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Card from './Card';
 
 function Vitrine() {
   return (
-    <div>
-      Isso vai ser uma vitrine
-    </div>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        margin: '50px 0',
+        padding: '0',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          borderRadius: '15px',
+          boxShadow: 1,
+          margin: '10px',
+          width: '50%',
+          padding: '10px',
+        }}
+      >
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </Box>
+    </Box>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Card from './components/Card';
+import Vitrine from './components/Vitrine';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Card />
+    <Vitrine />
   </React.StrictMode>,
   document.getElementById('root'),
 );
